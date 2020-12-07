30614d15dac276c7ea1498343049a48a5b35e660
damonbogich <damonbogich@gmail.com>
2020-12-07 14:27:48 -0500

    use dark mode written

--- src/components/Hooks/UseDarkMode.js
+++ src/components/Hooks/UseDarkMode.js
@@ -1,10 +1,16 @@
-// import {UseLocalStorage} from './UseLocalStorage';
-// import {useEffect} from 'react'
+import UseLocalStorage from './UseLocalStorage';
+import {useEffect} from 'react';
 
-// export default function UseDarkMode() {
-//     const [value, setValue] = UseLocalStorage('darkMode', false);
 
-//     if (value === true) {
-
-//     }
-// }
+export default function UseDarkMode() {
+    const [darkState, setDarkState] = UseLocalStorage('darkMode', false);
+    useEffect(() => {
+        if (darkState === true) {
+            document.body.classList.add('dark-mode')
+        } 
+        else {
+            document.body.classList.remove('dark-mode')
+        }
+    }, [darkState])
+    return[darkState, setDarkState]
+};
--- src/components/Hooks/UseLocalStorage.js
+++ src/components/Hooks/UseLocalStorage.js
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 
 export default function UseLocalStorage(key, initialValue) {
-    //setting up state
+    //grabs the value from local storage if its there
     const [storedValue, setStoredValue] = useState(() => {
         //sets variable to item in local storage 
         const item = window.localStorage.getItem(key);
         //if that variable was truthy, we set it to initial state, if not we set the initial value
         return item ? JSON.parse(item) : initialValue
     });
+    //puts value into local storage
     const setValue = value => {
         setStoredValue(value);
         window.localStorage.setItem(key, JSON.stringify(value))
